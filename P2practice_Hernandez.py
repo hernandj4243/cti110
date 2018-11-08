845fd3c3abd048f6fed0251134eefe5324111fc7
hernandj4243 <44876319+hernandj4243@users.noreply.github.com>
2018-11-08 12:35:07 -0500

    Add files via upload
    first submission

---
+++ P2practice_Hernandez.py
@@ -0,0 +1,19 @@
+#CYI 110
+#P@Practice
+#Jason Hernandez
+#11/08/18
+
+#ask user for their age
+#tell them how old they will be next year
+
+#get input number = int(input("enter number: "))
+#calculate the anwser( answer = + 1)
+#print output
+#print("your result is", answer)
+
+age = int(input("Enter age: "))
+ageNextYear =(age + 1)
+print("your age next year will be", ageNextYear)
+
+
+#(age) and (ageNextYear) are differnet variables
